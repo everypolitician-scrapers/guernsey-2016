232da7962dafbba8b949b79e5332ddc87f48da7f
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-11 15:06:57 +0100

    Adapt AllMembersPage for Guernsey
    Captures names and urls of members listed on Guernsey contact page. https://gov.gg/contactus

--- lib/all_members_page.rb
+++ lib/all_members_page.rb
@@ -3,23 +3,17 @@ require_rel 'page'
 
 class AllMembersPage < Page
   field :members do
-    member_table.css('tr').drop(1).map do |tr|
-      td = tr.css('td')
+    member_nodes.drop(1).map do |mem|
       {
-        name:    td[0].text.tidy,
-        faction: td[1].text.split(/,\s+/).map(&:tidy),
-        terms:   td[2].text.split(/,\s+/).map(&:tidy),
-        state:   td[3].text.split(/,\s+/).map(&:tidy),
-        url:     absolute_url(td[0].css('a/@href').text),
+        name:    mem.text.tidy,
+        url:     mem.xpath('a/@href').text,
       }
     end
   end
 
   private
 
-  def member_table
-    mt = noko.css('.tabelle') or raise 'No member table'
-    raise 'Too many member tables' if mt.count > 1
-    mt
+  def member_nodes
+    noko.xpath("id('subNavigation')/li[4]/ul//li")
   end
 end
