8a60bb94b250e6ed0eedb37626e73021699ef8d1
Oliver Denman <oliver@therewillbeanimals.com>
2016-10-12 10:51:01 +0100

    Add scraped page archiver

--- scraper.rb
+++ scraper.rb
@@ -1,21 +1,18 @@
 #!/bin/env ruby
 # encoding: utf-8
 require 'require_all'
-require 'open-uri/cached'
+# require 'open-uri/cached'
+require 'scraped_page_archive/open-uri'
+# OpenURI::Cache.cache_path = '.cache'
 require 'scraperwiki'
 
 require_rel 'lib'
 
-OpenURI::Cache.cache_path = '.cache'
 
 url = 'https://gov.gg/contactus'
 member_list = AllMembersPage.new(url).to_h
 
 warn "Found #{member_list[:members].count} members"
-
-member_pages = member_list[:members].map do |mem|
-  MemberPage.new(mem[:url]).to_h
-end
 
 member_list[:members].shuffle.each do |mem|
   member = MemberPage.new(mem[:url]).to_h
