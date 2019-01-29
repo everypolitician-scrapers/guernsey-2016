1eda4346ecbabc6defc86c94209483c8e9b6513b
Tony Bowden <tony@tmtm.com>
2019-01-29 13:35:20 +0000

    Inline all files
    We're not currently getting any benefit from having them split out.

--- scraper.rb
+++ scraper.rb
@@ -3,12 +3,66 @@
 # frozen_string_literal: true
 
 require 'require_all'
+require 'scraped'
 require 'scraperwiki'
 
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-require_rel 'lib'
+class AllMembersPage < Scraped::HTML
+  field :members do
+    member_nodes.drop(1).map do |mem|
+      {
+        name: mem.css('span').map(&:text).first.sub(/^Deputy/, '').tidy,
+        url:  mem.css('a/@href').text,
+      }
+    end
+  end
+
+  private
+
+  def member_nodes
+    noko.xpath("id('subNavigation')//li[@id]")
+  end
+end
+
+class MemberPage < Scraped::HTML
+  field :name do
+    noko.xpath("id('CenterContainer')/h1").text.tidy
+  end
+
+  field :area do
+    noko.xpath("id('Breadcrumb')/li[last() - 1]").text.sub('Representatives', '').tidy
+  end
+
+  field :party do
+    'Independent'
+  end
+
+  field :email do
+    noko.xpath('//p/a[contains(@href, "mailto:")]/@href').text.gsub('mailto:', '').tidy
+  end
+
+  field :website do
+    noko.xpath('//div[@id="contactDetails"]//span[.="Website:"]/following-sibling::a/@href').text.tidy
+  end
+
+  field :twitter do
+    noko.xpath('//div[@id="contactDetails"]//span[@class="icon-tw"]/../@href').text.tidy
+  end
+
+  field :facebook do
+    noko.xpath('//div[@id="contactDetails"]//span[@class="icon-fb"]/../@href').text.tidy
+  end
+
+  field :term do
+    2016
+  end
+
+  field :source do
+    url.to_s
+  end
+end
 
 def scraper(h)
   url, klass = h.to_a.first
